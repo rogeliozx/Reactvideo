ad630b549bd654b5dfaae016a4dbf6a785d9ac7e
rogeliozx <rogelioltz@gmail.com>
2019-02-27 12:49:19 -0700

    The buttons of video can play and stop

--- src/Player/Container/video-player.js
+++ src/Player/Container/video-player.js
@@ -6,7 +6,14 @@ import PlayPause from '../Components/play-pause';
 class VideoPlayer extends Component {
   state={
     pause:true,
+    duration:0,
   }
+  handleLoadedMetadata =(event)=>{
+    this.video= event.target;
+    this.setState({
+      duration:this.video.duration
+    })
+    }
   componentDidMount(){
     this.setState({
       pause:(!this.props.autoplay)
@@ -16,9 +23,7 @@ class VideoPlayer extends Component {
 this.setState({
   pause:!this.state.pause
 })
-
-
-  }
+ }
     render() {
       return (
         <VideoPlayerLayout handleSet={this.props}>
@@ -31,7 +36,10 @@ this.setState({
         />
          <Video
          autoplay={this.props.autoplay}
+         pause={this.state.pause}
+         onLoadedMetadata={this.handleLoadedMetadata}
          src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+        
          />
         </VideoPlayerLayout>
       )
